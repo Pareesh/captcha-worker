1567c71b46062773e8e4ff9477029d201a902653
Pareesh Gupta <pareesh@Pareeshs-MacBook-Pro.local>
2023-03-26 20:15:00 +0530

    Implement recaptchav2 with worker

--- src/index.js
+++ src/index.js
@@ -1,34 +1,30 @@
 function getOrigin(headers) {
-  const referrer = headers ? headers.get('Referer') : '';
-  if (referrer) {
-    const { origin } = new URL(referrer);
-    return origin;
-  }
-  return '';
+  const origin = headers ? headers.get('origin') : '';
+  return origin;
 }
 
 function isValidOrigin(origin) {
-  console.log(origin);
-  return origin === 'https://recaptchav3--afb-gdrive--pareesh.hlx.page' || origin === 'https://recaptchav3--afb-gdrive--pareesh.hlx.live';
+  const allowedOrigins = [
+    'https://recaptchav3--afb-gdrive--pareesh.hlx.page',
+    'https://recaptchav3--afb-gdrive--pareesh.hlx.live',
+    'https://recaptchav2--afb-gdrive--pareesh.hlx.page',
+    'https://recaptchav2--afb-gdrive--pareesh.hlx.live'
+  ];
+
+  return allowedOrigins.includes(origin);
 }
 
 function getResponseHeaders(origin) {
   const headers = new Headers();
   headers.set('Access-Control-Allow-Origin', origin)
   headers.set('Access-Control-Allow-Methods', 'POST')
-  headers.set(
-    'Access-Control-Allow-Headers',
-    'access-control-allow-headers, g-recaptcha'
-  )
-  headers.set('Access-Control-Max-Age', 86400)
+  headers.set('Access-Control-Allow-Headers', 'access-control-allow-headers, g-recaptcha, Content-Type')
+  headers.set('Access-Control-Max-Age', 86400);
   return headers;
 }
 
 function getResponse(body, status, headers) {
-  return new Response(body, {
-    status: status,
-    headers: headers
-  });
+  return new Response(body, { status: status, headers: headers });
 }
 
 async function handleRequest(request, env, origin) {
@@ -50,22 +46,38 @@ async function handleRequest(request, env, origin) {
       return getResponse('No Recaptcha Token', 400, responseHeaders);
     }
 
-    const secret = env.SECRET;
+    const { type } = await request.json();
 
-    const response = await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${recaptchaToken}`, {
-        method: 'POST'
-      }
-    )
+    let response = new Response(JSON.stringify({ success: false }), { status: 400 });
+
+    if (type === 'recaptchav3') {
+      const secret = env.SECRET_V3;
+
+      response = await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${recaptchaToken}`, {
+          method: 'POST'
+        }
+      )
+    } else if (type === 'recaptchav2') {
+      const secret = env.SECRET_V2;
+
+      response = await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${recaptchaToken}`, {
+          method: 'POST'
+        }
+      )
+    } else {
+      return getResponse('Captcha type not specified', 400, responseHeaders);
+    }
+
 
     const body = await response.json();
 
     console.log(body);
 
-    if (!body.success) {
-      return getResponse('Fail', 400, responseHeaders);
+    if (body.success) {
+      return getResponse('Pass', 202, responseHeaders);
     }
 
-    return getResponse('Pass', 202, responseHeaders);
+    return getResponse('Fail', 400, responseHeaders);
   } catch(err) {
     console.error(err);
     return getResponse(err.stack, 500, responseHeaders);  
@@ -75,12 +87,7 @@ async function handleRequest(request, env, origin) {
 async function get(request, env) {
   const origin = getOrigin(request.headers);
   const isValid = isValidOrigin(origin);
-
-  if (isValid) {
-    return await handleRequest(request, env, origin);
-  } else {
-    return new Response('', { status: 404 });
-  }
+  return isValid ? await handleRequest(request, env, origin) : new Response('', { status: 404 });
 }
 
 export default {
